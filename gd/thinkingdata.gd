bf7aa12cb047c7b2aea1c172f73cb605874f6417
slabgames <55172860+slabgames@users.noreply.github.com>
2023-12-09 18:26:40 +0800

    fix naming

--- gd/thinkingdata.gd
+++ gd/thinkingdata.gd
@@ -3,21 +3,21 @@ extends Node
 var _thinkingdata = null
 
 func _ready():
-    if Engine.has_singleton("GodotThinkingData"):
-        _thinkingdata = Engine.get_singleton("GodotThinkingData")
-    else:
-        push_warning('GodotThinkingData plugin not found!')
+	if Engine.has_singleton("GodotThinkingData"):
+		_thinkingdata = Engine.get_singleton("GodotThinkingData")
+	else:
+		push_warning('GodotThinkingData plugin not found!')
 
 
 func init(appId: String, serverUrl:String) -> void:
-    if _thinkingdata != null:
-        _thinkingdata.init(appId, serverUrl)
-        print('GodotThinkingData plugin inited!')
-        
+	if _thinkingdata != null:
+		_thinkingdata.init(appId, serverUrl)
+		print('GodotThinkingData plugin inited!')
+		
 func login(userId:String)->void:
-    if _thinkingdata != null:
-    	_thinkingdata.login(userId)
+	if _thinkingdata != null:
+		_thinkingdata.login(userId)
 
-func sendEvent(key:String, value:String, eventName:String)
+func sendEvent(key:String, value:String, eventName:String):
 	if _thinkingdata != null:
 		_thinkingdata.sendEvent(key,value,eventName)
